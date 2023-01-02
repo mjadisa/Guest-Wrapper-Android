3704efe35defdb2d2671a2d4abedc116f79fbc49
mjadisa <mujeeb.adisa@gmail.com>
2023-01-02 13:31:04 +0000

    update jre location

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(echo ""; echo "org.gradle.java.home=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/jre") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+(echo ""; echo "org.gradle.java.home=/Users/runner/hostedtoolcache/Java_Temurin-Hotspot_jdk/8.0.352-8/x64/Contents/Home/jre") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
