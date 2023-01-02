e0998ad65fb5b0897a0db17d5cf8095a56907e21
mjadisa <mujeeb.adisa@gmail.com>
2023-01-02 13:26:59 +0000

    update jdk location

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-(echo ""; echo "org.gradle.java.home=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
+(echo ""; echo "org.gradle.java.home=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/jre") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
