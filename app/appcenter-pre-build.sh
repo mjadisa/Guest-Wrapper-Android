2ee309d115243399466f5eeed7335bfc1884ffd6
mjadisa <mujeeb.adisa@gmail.com>
2023-01-02 13:51:19 +0000

    change java version to 11

--- app/appcenter-pre-build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-(echo ""; echo "org.gradle.java.home=/Users/runner/hostedtoolcache/Java_Temurin-Hotspot_jdk/11.0.11+9/x64/Contents/Home/jre") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
